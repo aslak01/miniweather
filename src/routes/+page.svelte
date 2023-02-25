267f7c603f5d581bc9091e7b7a3f666336c2fa0d
aslak <aslakrb@gmail.com>
2023-02-25 17:41:02 +0100

    using icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { env } from '$env/dynamic/public';
+	import wi from '$lib/weathericon/svg';
 
 	const LAT = env.PUBLIC_LAT,
 		LON = env.PUBLIC_LON;
 
-	const getForecast = async () => {
+	const getForecast = async (lat: string, lon: string) => {
 		const req = await fetch(
-			`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${LAT}&lon=${LON}`
+			`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`
 		);
 		const res = await req.json();
-		const forecast = res.properties.timeseries;
-		console.log(forecast);
+		const forecasts = res.properties.timeseries;
+		// TODO Improve forecast selection?
+		const currForecast = forecasts[1];
+		const now = currForecast.data.instant;
+		const coming = currForecast.data.next_6_hours;
+
+		return coming.summary.symbol_code;
 	};
 
-	onMount(() => {
-		getForecast();
+	let symbol = '';
+
+	$: console.log('symbol', symbol);
+
+	onMount(async () => {
+		LAT && LON ? (symbol = await getForecast(LAT, LON)) : '';
 	});
 </script>
 
 <div>
 	<p>Weather</p>
+  <img src={wi[symbol]} />
+	{symbol}
 </div>
