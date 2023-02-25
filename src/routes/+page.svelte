5c12f0ea2a585ec470a76f1ba04b838ce1ce4526
as <aslakrb@gmail.com>
2023-02-25 13:10:20 +0100

    refactor to kit
    fix

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { env } from '$env/dynamic/public';
+
+	const LAT = env.PUBLIC_LAT,
+		LON = env.PUBLIC_LON;
+
+	const getForecast = async () => {
+		const req = await fetch(
+			`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${LAT}&lon=${LON}`
+		);
+		const res = await req.json();
+		const forecast = res.properties.timeseries;
+		console.log(forecast);
+	};
+
+	onMount(() => {
+		getForecast();
+	});
+</script>
+
+<div>
+	<p>Weather</p>
+</div>
