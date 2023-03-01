f811be4d2c42857591bd7613766929908c14307f
aslak <aslakrb@gmail.com>
2023-03-01 22:43:12 +0100

    swap dev

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,25 +7,25 @@ import { getWeather, getIcon, getRain, getTemps } from '$lib/functions/index';
 import { dummyIcon, dummyTemps, dummyRain } from '$lib/testing';
 
 const LAT = env.PUBLIC_LAT,
-	LON = env.PUBLIC_LON,
-	DEV = import.meta.env.DEV;
+  LON = env.PUBLIC_LON,
+  DEV = import.meta.env.DEV;
 
 export const load: PageServerLoad = async (_event) => {
-	if (LAT && LON && DEV) {
-		const fetch = await getWeather(LAT, LON).then((res: Timeseries[]) => {
-			console.log(res);
-			return {
-				iconkey: getIcon(res),
-				rain: getRain(res, 6),
-				temps: getTemps(res, 6)
-			};
-		});
-		return fetch;
-	} else {
-		return {
-			iconkey: dummyIcon,
-			rain: dummyRain,
-			temps: dummyTemps
-		};
-	}
+  if (LAT && LON && !DEV) {
+    const fetch = await getWeather(LAT, LON).then((res: Timeseries[]) => {
+      console.log(res);
+      return {
+        iconkey: getIcon(res),
+        rain: getRain(res, 6),
+        temps: getTemps(res, 6)
+      };
+    });
+    return fetch;
+  } else {
+    return {
+      iconkey: dummyIcon,
+      rain: dummyRain,
+      temps: dummyTemps
+    };
+  }
 };
