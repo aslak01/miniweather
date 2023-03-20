dc973abe15fd8096d87dfd15368927b5aa16dfcb
as <aslak.bakkeland@medialaben.no>
2023-03-20 23:44:41 +0100

    prettier

--- src/lib/functions/trainData.ts
+++ src/lib/functions/trainData.ts
@@ -1,18 +1,18 @@
 import type {
-  TrainData,
-  RelevantTrainInfo,
-  SortedTrainData,
-  StopToStopGeometries
+	TrainData,
+	RelevantTrainInfo,
+	SortedTrainData,
+	StopToStopGeometries
 } from '../types';
 import { dateStringToMinFromNow, timeDiffInMin } from './date';
 
 export const getTrains = async (): Promise<TrainData[]> => {
-  const myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
-  myHeaders.append('ET-Client-Name', 'personlig-infoskjerm');
+	const myHeaders = new Headers();
+	myHeaders.append('Content-Type', 'application/json');
+	myHeaders.append('ET-Client-Name', 'personlig-infoskjerm');
 
-  var graphql = JSON.stringify({
-    query: `
+	var graphql = JSON.stringify({
+		query: `
       {
         stopPlace(
           id: "NSR:StopPlace:418"
@@ -51,71 +51,69 @@ export const getTrains = async (): Promise<TrainData[]> => {
         }
       }
     `
-  });
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: graphql
-  };
+	});
+	const requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: graphql
+	};
 
-  const res = await fetch(
-    'https://api.entur.io/journey-planner/v3/graphql',
-    requestOptions
-  );
-  const json = await res.json();
-  return json.data.stopPlace.estimatedCalls;
+	const res = await fetch(
+		'https://api.entur.io/journey-planner/v3/graphql',
+		requestOptions
+	);
+	const json = await res.json();
+	return json.data.stopPlace.estimatedCalls;
 };
 
 export const extractLineNamesAndRelTime = (
-  train: TrainData
+	train: TrainData
 ): RelevantTrainInfo => {
-  const expected = train.expectedDepartureTime;
-  const aimed = train.aimedDepartureTime;
-  const diff = timeDiffInMin(expected, aimed);
-  return {
-    line: train.serviceJourney.line.publicCode,
-    display: train.destinationDisplay.frontText,
-    time: dateStringToMinFromNow(expected),
-    diff
-  };
+	const expected = train.expectedDepartureTime;
+	const aimed = train.aimedDepartureTime;
+	const diff = timeDiffInMin(expected, aimed);
+	return {
+		line: train.serviceJourney.line.publicCode,
+		display: train.destinationDisplay.frontText,
+		time: dateStringToMinFromNow(expected),
+		diff
+	};
 };
 
-export const splitAndCleanTrains = (
-  data: TrainData[]
-): SortedTrainData => {
-  const askerBeforeOslo = (d: TrainData) => {
-    const indexOfStation = (sts: TrainData, x: string): number =>
-      sts.serviceJourney.journeyPattern.stopToStopGeometries
-        .map((s: StopToStopGeometries) => s.toQuay.name)
-        .indexOf(x);
-    const askerIndex = indexOfStation(d, 'Asker stasjon');
-    const osloIndex = indexOfStation(d, 'Oslo S');
-    return askerIndex < osloIndex;
-  };
-  const tooSoon = (d: RelevantTrainInfo) => {
-    return d.time > 15;
-  };
-  const tooLate = (d: RelevantTrainInfo) => {
-    return d.time < 60
-  }
-  const noFlytog = (d: RelevantTrainInfo) => {
-    return d.line.includes('FLY') === false;
-  };
-  const northbound = data
-    .filter(askerBeforeOslo)
-    .map(extractLineNamesAndRelTime)
-    .filter(tooSoon)
-    .filter(tooLate)
-    .filter(noFlytog);
-  const southbound = data
-    .filter((train: TrainData) => askerBeforeOslo(train) === false)
-    .map(extractLineNamesAndRelTime)
-    .filter(tooSoon)
-    .filter(tooLate)
-    .filter(noFlytog);
+export const splitAndCleanTrains = (data: TrainData[]): SortedTrainData => {
+	const askerBeforeOslo = (d: TrainData) => {
+		const indexOfStation = (sts: TrainData, x: string): number =>
+			sts.serviceJourney.journeyPattern.stopToStopGeometries
+				.map((s: StopToStopGeometries) => s.toQuay.name)
+				.indexOf(x);
+		const askerIndex = indexOfStation(d, 'Asker stasjon');
+		const osloIndex = indexOfStation(d, 'Oslo S');
+		return askerIndex < osloIndex;
+	};
+	const tooSoon = (d: RelevantTrainInfo) => {
+		return d.time > 15;
+	};
+	const tooLate = (d: RelevantTrainInfo) => {
+		return d.time < 60;
+	};
+	const noFlytog = (d: RelevantTrainInfo) => {
+		return d.line.includes('FLY') === false;
+	};
+	const northbound = data
+		.filter(askerBeforeOslo)
+		.map(extractLineNamesAndRelTime)
+		.filter(tooSoon)
+		.filter(tooLate)
+		.filter(noFlytog);
+	const southbound = data
+		.filter((train: TrainData) => askerBeforeOslo(train) === false)
+		.map(extractLineNamesAndRelTime)
+		.filter(tooSoon)
+		.filter(tooLate)
+		.filter(noFlytog);
 
-  return {
-    northbound,
-    southbound
-  };
+	return {
+		northbound,
+		southbound
+	};
 };
