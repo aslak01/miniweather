ae6c4c95a9062ce06fe9e213cc6b7a89a9eb84fd
aslak <aslakrb@gmail.com>
2023-07-31 13:30:14 +0200

    add resolution preview on dev

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,31 @@
   import "@fontsource/inter";
   import "@fontsource/inter/800.css";
   import "../app.css";
+  import { dev } from "$app/environment";
 </script>
 
 <main>
-  <slot />
+  {#if dev}
+    <div class="square">
+      <slot />
+    </div>
+  {:else}
+    <slot />
+  {/if}
 </main>
 
 <style>
   main {
     max-width: 480px;
     max-height: 280px;
+    position: relative;
+  }
+  .square {
+    height: 280px;
+    width: 480px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: 1px solid red;
   }
 </style>
