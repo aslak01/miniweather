b1658e35230f31932e02f38ca2fe4c08fb9be385
as <aslak.bakkeland@medialaben.no>
2023-11-24 21:35:33 +0100

    add layout help

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,26 +7,34 @@
 
 <main>
   {#if dev}
-    <div class="square">
-      <slot />
+    <div class="centerer">
+      <div class="square size-constrainer">
+        <slot />
+      </div>
     </div>
   {:else}
-    <slot />
+    <div class="size-constrainer">
+      <slot />
+    </div>
   {/if}
 </main>
 
 <style>
-  main {
-    position: relative;
-    max-width: 480px;
-    max-height: 280px;
+  main,
+  :global(body, html) {
+    height: 100%;
+    min-height: 100dvh;
   }
-  .square {
-    position: absolute;
-    top: 0;
-    left: 0;
-    border: 1px solid red;
+  .size-constrainer {
     width: 480px;
     height: 280px;
+  }
+  .square {
+    border: 2px solid red;
+  }
+  .centerer {
+    display: grid;
+    place-content: center;
+    height: 100%;
   }
 </style>
